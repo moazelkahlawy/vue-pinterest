d6c45b9de9301d5235c6b89b522add94915ae577
moazelkahlawy <mezo.abdo65@gmail.com>
2021-08-30 13:11:09 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,189 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <topNav />
+    <div class="px-20 w-full flex flex-wrap">
+      <div v-for="(arr, index) in subArrays" :key="index" class="w-1/5" >
+        <div v-for="(card, i) in arr" :key="i" class="w-full">
+        <div class="w-full p-2">
+        <Card :src="`${card.src}`" :board="`${card.recommended}`" class="w-full h-full"/>
+        <div class="text-sm leading-tight pt-2">
+          <p class="font-bold">{{card.title}}</p>
+          <p>{{card.user}}</p>
+        </div>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import "./assets/tailwind.css";
+import topNav from "./components/topNav.vue";
+import Card from "./components/Card.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    topNav,
+    Card
+  },
+  computed: {
+    subArrays() {
+      var length = Math.ceil(this.cards.length / 5);
+      const result = new Array(length)
+      .fill()
+      .map((i) => {
+        console.log(i);
+        return this.cards.splice(0, length)
+      })
+      return result
+    }
+  },
+  data() {
+    return {
+      cards: [
+        {
+          src: '1.jpg',
+          title: 'Title 1',
+          user: 'User 1',
+          recommended: 'Travel'
+        },
+        {
+          src: '2.jpg',
+          title: 'Title 2',
+          user: 'User 2',
+          recommended: 'Travel'
+        },
+        {
+          src: '3.jpg',
+          title: 'Title 3',
+          user: 'User 3',
+          recommended: 'Travel'
+        },
+        {
+          src: '4.jpg',
+          title: 'Title 4',
+          user: 'User 4',
+          recommended: 'Travel'
+        },
+        {
+          src: '5.jpg',
+          title: 'Title 5',
+          user: 'User 5',
+          recommended: 'Travel'
+        },
+        {
+          src: '6.jpg',
+          title: 'Title 6',
+          user: 'User 6',
+          recommended: 'Travel'
+        },
+        {
+          src: '7.jpg',
+          title: 'Title 7',
+          user: 'User 7',
+          recommended: 'Travel'
+        },
+        {
+          src: '8.jpg',
+          title: 'Title 8',
+          user: 'User 8',
+          recommended: 'Travel'
+        },
+        {
+          src: '9.jpg',
+          title: 'Title 9',
+          user: 'User 9',
+          recommended: 'Travel'
+        },
+        {
+          src: '10.jpg',
+          title: 'Title 10',
+          user: 'User 10',
+          recommended: 'Travel'
+        },
+        {
+          src: '11.jpg',
+          title: 'Title 11',
+          user: 'User 11',
+          recommended: 'Travel'
+        },
+        {
+          src: '12.jpg',
+          title: 'Title 12',
+          user: 'User 12',
+          recommended: 'Travel'
+        },
+        {
+          src: '13.jpg',
+          title: 'Title 13',
+          user: 'User 13',
+          recommended: 'Travel'
+        },
+        {
+          src: '1.jpg',
+          title: 'Title 1',
+          user: 'User 1',
+          recommended: 'Travel'
+        },
+        {
+          src: '2.jpg',
+          title: 'Title 2',
+          user: 'User 2',
+          recommended: 'Travel'
+        },
+        {
+          src: '3.jpg',
+          title: 'Title 3',
+          user: 'User 3',
+          recommended: 'Travel'
+        },
+        {
+          src: '4.jpg',
+          title: 'Title 4',
+          user: 'User 4',
+          recommended: 'Travel'
+        },
+        {
+          src: '5.jpg',
+          title: 'Title 5',
+          user: 'User 5',
+          recommended: 'Travel'
+        },
+        {
+          src: '6.jpg',
+          title: 'Title 6',
+          user: 'User 6',
+          recommended: 'Travel'
+        },
+        {
+          src: '7.jpg',
+          title: 'Title 7',
+          user: 'User 7',
+          recommended: 'Travel'
+        },
+        {
+          src: '8.jpg',
+          title: 'Title 8',
+          user: 'User 8',
+          recommended: 'Travel'
+        },
+        {
+          src: '9.jpg',
+          title: 'Title 9',
+          user: 'User 9',
+          recommended: 'Travel'
+        },
+        {
+          src: '10.jpg',
+          title: 'Title 10',
+          user: 'User 10',
+          recommended: 'Travel'
+        },
+      ]
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/topNav.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="px-3 py-4 flex items-center justify-between w-full">
+    <div class="flex items-center">
+      <button
+        class="rounded-full hover:bg-light px-2 py-1 flex items-center justify-center"
+        type="button"
+        name="button"
+      >
+        <img src="../../public/logo.png" alt="logo" class="w-9" />
+      </button>
+      <button
+        v-for="(item, i) in tabItems"
+        :key="i"
+        @click="selectedTab == item.key"
+        :class="`${
+          selectedTab === item.key ? 'bg-dark text-white' : 'text-dark hover:bg-light'
+        } rounded-full py-3 px-4 font-semibold`"
+        type="button"
+        name="button"
+      >
+        {{ item.name }}
+      </button>
+    </div>
+    <div class="flex-grow relative">
+      <span class="material-icons absolute mt-3 ml-3 text-muted">search</span>
+      <input
+        class="text-muted w-full hover:bg-lightHover rounded-full bg-light py-3 pr-3 pl-10"
+        placeholder="Search"
+        type="text"
+      />
+    </div>
+    <div class="flex items-center">
+      <button
+        type="button"
+        name="button"
+        class="relative rounded-full hover:bg-light flex items-center justify-center p-3"
+      >
+        <span class="material-icons text-2xl text-muted">notifications</span>
+        <div
+          class="rounded-full h-5 w-5 bg-primary text-white flex items-center justify-center absolute mb-5 ml-5"
+        >
+          4
+        </div>
+      </button>
+      <button
+        type="button"
+        name="button"
+        class="relative rounded-full hover:bg-light flex items-center justify-center p-3"
+      >
+        <span class="material-icons text-2xl text-muted">textsms</span>
+        <div
+          class="rounded-full h-5 w-5 bg-primary text-white flex items-center justify-center absolute mb-5 ml-5"
+        >
+          1
+        </div>
+      </button>
+      <button
+        class="rounded-full hover:bg-light p-2 flex items-center justify-center"
+        type="button"
+        name="button"
+      >
+        <img src="../../public/avatar.jpg" alt="logo" class="w-9 rounded-full" />
+      </button>
+        <button type="button" name="button" class="w-6 h-6 rounded-full hover:bg-light flex items-center justify-center">
+        <span class="material-icons text-muted">expand_more</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "topNav",
+  data() {
+    return {
+      tabItems: [
+        { name: "Home", key: "home" },
+        { name: "Today", key: "today" },
+      ],
+      selectedTab: "home",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
